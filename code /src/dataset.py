20bb775d2eb04568ace968ed407f6ba83dd803ae
NikTimes <kikeheine@gmail.com>
2026-03-09 15:33:26 +0000

    Added full_dolution functionality on dataset object

--- code /src/dataset.py
+++ code /src/dataset.py
@@ -1,4 +1,8 @@
-from utils import *
+try:
+    from src.utils import ODEsolver
+except ModuleNotFoundError:
+    from utils import ODEsolver
+
 import numpy as np 
 
 import torch
@@ -15,6 +19,7 @@ class ODEIterableDataset(IterableDataset):
         sampler       (object) : Sampler Object with __call__ method
         t_span        (tuple)  : Tuple including initial and end integration times
         method        (String) : solve_ivp numerical integration method
+        full_solution (String) : Whether or not to output full solution or solution at time tf
 
     """
 
@@ -23,16 +28,18 @@ class ODEIterableDataset(IterableDataset):
                  system_class,
                  sampler, 
                  t_span,
-                 method     ="RK45"):
+                 method        = "RK45",
+                 full_solution = False):
 
         self.start      = 0
         self.end        = size
         self.size       = size
 
-        self.system     = system_class
-        self.sampler    = sampler
-        self.t_span     = t_span
-        self.method     = method
+        self.system        = system_class
+        self.sampler       = sampler
+        self.t_span        = t_span
+        self.method        = method
+        self.full_solution = full_solution
 
         self.solver     = ODEsolver(system_class)
     
@@ -60,19 +67,32 @@ class ODEIterableDataset(IterableDataset):
             t0, tf = self.t_span
             t      = np.random.uniform(t0, tf)
 
-            # Solve only until t
-            sol    = self.solver.solve((t0, t),     # Solve only until time t
-                                        y0, 
-                                        self.method,
-                                        t_eval=[t]) # Only Returns solution at t
-                                        
+            # Evaluate everywhere
+            if self.full_solution:
+                sol    = self.solver.solve((t0, tf),   
+                                            y0, 
+                                            self.method) 
+                
+                # Store full solution
+                y_t    = sol.y[:]
+                t_eval = sol.t[:]
+                
             
-
-            y_t = sol.y[:, -1]
+            # Solve only until time t
+            else:
+                sol    = self.solver.solve((t0, t),     
+                                            y0, 
+                                            self.method,
+                                            t_eval=[t]) # Only Returns solution at t
+                
+                # Store solution at time t
+                y_t    = sol.y[:, -1]
+                t_eval = sol.t[:] 
+                                        
 
             I = torch.tensor(y0,    dtype=torch.float32) # Initial Condition
-            t = torch.tensor([t],   dtype=torch.float32) # output time 
-            y = torch.tensor(y_t, dtype=torch.float32) # y_I(t)
+            t = torch.tensor(t_eval,   dtype=torch.float32) # output time 
+            y = torch.tensor(y_t,   dtype=torch.float32) # y_I(t)
 
 
             yield I, t, y
@@ -81,16 +101,3 @@ class ODEIterableDataset(IterableDataset):
         return self.size
 
 
-
-
-"""
-k = 2.0
-c = 0.5
-system = harm_osc([k, c])
-
-sampler = LatinHypercubeSampler(
-    dimensions=2,
-    lows      = [-1.0, -1.0],
-    highs     = [1.0, 1.0]
-)
-"""
